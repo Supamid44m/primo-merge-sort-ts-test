3084b286ff7043f7edb975e395c9d0d1a3587f27
Supamid44m <supamid780@gmail.com>
2026-07-03 00:56:34 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+
+
+`
+ given
+     collection_1 already sorted from max to min
+     collection_2, collection_3 already sorted from min(0) to max
+`
+
+const collection_1 :number[]=[]
+const collection_2: number[] = [], collection_3: number[] = []
+
+export function mergeSort() {
+
+}
+
+
+export function bubbleSort(array: number[]) {
+
+}
+
+console.log("test")
---
+++ src/test/index.test.ts
@@ -0,0 +1,8 @@
+// import {hello} from "../index";
+//
+// describe('test', () => {
+//     it('hello returns "hello"', () => {
+//         const result = hello();
+//         expect(result).toEqual("hello");
+//     });
+// })
