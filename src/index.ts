6178e6707741e93e7771229f74d75be6ffe78b49
Supamid44m <supamid780@gmail.com>
2026-07-03 01:16:14 +0700

    feat: implement bubble sort

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,37 @@
-
-
 `
  given
      collection_1 already sorted from max to min
      collection_2, collection_3 already sorted from min(0) to max
 `
 
-const collection_1 :number[]=[]
-const collection_2: number[] = [], collection_3: number[] = []
+const collection_1: number[] = [5,4,3,2,1]
+const collection_2: number[] = [1,2,3,4], collection_3: number[] = [1,2,3]
 
-export function mergeSort() {
+export function mergeSort(arr1:number[], arr2: Array<number>,arr3: Array<number>) {
 
+    let mergeArray = []
+
+    mergeArray = [...collection_1, ...collection_2, ...collection_3]
+
+    return bubbleSort(mergeArray)
 }
 
 
-export function bubbleSort(array: number[]) {
+export function bubbleSort(array: number[]): number[] {
 
+    let temp = 0
+
+    for(let i=0;i<array.length; i++) {
+        for (let j = 0; j < array.length-i-1; j++) {
+            if(array[j]>array[j+1]){
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+            }
+        }
+    }
+
+    return array;
 }
 
-console.log("test")
+console.log(mergeSort(collection_1,collection_2,collection_3))
