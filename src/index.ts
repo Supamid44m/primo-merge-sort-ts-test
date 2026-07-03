518fc04b6782474416cb6b849324b1ef9696565a
Supamid44m <supamid780@gmail.com>
2026-07-03 16:13:35 +0700

    feat: add test case and merge 2 ascending array

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,72 @@
      collection_2, collection_3 already sorted from min(0) to max
 `
 
-const collection_1: number[] = [5,4,3,2,1]
-const collection_2: number[] = [1,2,3,4], collection_3: number[] = [1,2,3]
+const collection_1: number[] = [5,4,3,2,1,-1]
+const collection_2: number[] = [1, 2, 3, 4], collection_3: number[] = [0,1,2,3,4,5]
 
-export function mergeSort(arr1:number[], arr2: Array<number>,arr3: Array<number>) {
+export function mergeAndSort(arr1: number[], arr2: Array<number>, arr3: Array<number>) {
 
     let mergeArray = []
 
-    mergeArray = [...collection_1, ...collection_2, ...collection_3]
+    if (arr1.length === 0 && arr2.length === 0 && arr3.length === 0) {
+        return []
+    }
+
+    if (arr1.length === 0 && arr3.length === 0) {
+        return arr2;
+    }
+
+    if (arr1.length === 0 && arr2.length === 0) {
+        return arr3;
+    }
+
+    if (arr1.length===0) {
+        return mergeSort2AscendingArray(arr2, arr3)
+    }
+
+    mergeArray = [...arr1, ...arr2, ...arr3]
 
     return bubbleSort(mergeArray)
 }
 
 
+export function mergeSort2AscendingArray(arr1: number[], arr2: number[]) {
+    const mergeArray: number[] = []
+    let i = 1
+    let j = 1
+    let arr1Item = arr1[0]
+    let arr2Item = arr2[0]
+
+    if(arr1.length === 0 ) {
+        return arr2
+    }
+    if(arr2.length ===0){
+        return arr1
+    }
+
+    while (arr1Item !== undefined|| arr2Item !== undefined) {
+        if(arr2Item === undefined ||arr1Item <= arr2Item) {
+            mergeArray.push(arr1Item)
+            arr1Item = arr1[i]
+            i++
+        }else{
+            mergeArray.push(arr2Item)
+            arr2Item = arr2[j]
+            j++
+        }
+
+    }
+   return mergeArray
+}
+
 export function bubbleSort(array: number[]): number[] {
-
     let temp = 0
-
-    for(let i=0;i<array.length; i++) {
-        for (let j = 0; j < array.length-i-1; j++) {
-            if(array[j]>array[j+1]){
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
                 temp = array[j]
-                array[j] = array[j+1]
-                array[j+1] = temp
+                array[j] = array[j + 1]
+                array[j + 1] = temp
             }
         }
     }
@@ -34,4 +77,4 @@ export function bubbleSort(array: number[]): number[] {
     return array;
 }
 
-console.log(mergeSort(collection_1,collection_2,collection_3))
+console.log(mergeAndSort(collection_1, collection_2, collection_3))
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,8 +1,43 @@
-// import {hello} from "../index";
-//
-// describe('test', () => {
-//     it('hello returns "hello"', () => {
-//         const result = hello();
-//         expect(result).toEqual("hello");
-//     });
-// })
+import {mergeAndSort} from "../index";
+
+
+const collection_1: number[] = [5, 4, 3, 2, 1]
+const collection_2: number[] = [1, 2, 3, 4], collection_3: number[] = [1, 2, 3]
+const mergeAllCollection = [...collection_1,...collection_2,...collection_3]
+
+
+describe('Merge and sort  Array', () => {
+    it('should be merge all collection and sort ', () => {
+        const result = mergeAndSort(collection_1, collection_2, collection_3)
+
+        expect(result).toEqual(mergeAllCollection.sort((a, b) => a - b))
+    })
+
+    it('should merge collection 2 and 3', () => {
+        const result = mergeAndSort([],collection_2, collection_3)
+
+        expect(result).toEqual([...collection_2,...collection_3].sort((a, b) => a - b))
+    })
+
+
+    it('should return collection 1 with sorted', () => {
+        const result = mergeAndSort(collection_1, [],[])
+        expect(result).toEqual(collection_1.sort((a, b) => a - b))
+    })
+
+    it('should return collection2',()=>{
+        const result = mergeAndSort([], collection_2, [])
+
+        expect(result).toEqual(collection_2)
+    })
+
+    it('should return collection3',()=>{
+        const result = mergeAndSort([],[],collection_3)
+        expect(result).toEqual(collection_3)
+    })
+
+    it('should return empty array', () => {
+        const result = mergeAndSort([],[],[])
+        expect(result).toEqual([])
+    })
+})
